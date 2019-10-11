e8fe359fb09b9ea1b6ddff0ef2fc9039ac65c060
whyza <whyzaa@163.com>
2019-10-11 17:10:19 +0800

    修复豆瓣图片403

--- src/views/douban/douban.vue
+++ src/views/douban/douban.vue
@@ -1,24 +1,26 @@
 <template>
-  <div id="container"  v-loading="listLoading" element-loading-text="Loading">
+  <div id="container" v-loading="listLoading" element-loading-text="Loading">
     <ul id="ul-movie">
       <li id="movie" v-for="movie in moviedata" :key="movie.id">
-        <img :src="movie.images.small">
+        <img :src="getImages(movie.images.small)" />
         <div class="right-content">
-          <div class="title">{{ movie.title}}</div>
+          <div class="title">片名：{{ movie.title}}</div>
           <div class="type mt5">
             <span v-for="(genres,index) in movie.genres" :key="genres">
-              {{genres}}
+              类型：{{genres}}
               <span v-show="index < movie.genres.length-1">/</span>
             </span>
           </div>
           <div class="direct mt5">
-            <span v-for="(director,index) in movie.directors">
+            导演：
+            <span v-for="(director,index) in movie.directors" :key="director">
               {{director.name}}
               <span v-show="index < movie.directors.length-1">/</span>
             </span>
           </div>
           <div class="actor mt5">
-            <span v-for="(cast,index) in movie.casts">
+            演员：
+            <span v-for="(cast,index) in movie.casts" :key="cast">
               {{cast.name}}
               <span v-show="index < movie.casts.length-1">/</span>
             </span>
@@ -28,48 +30,64 @@
     </ul>
     <el-pagination
       v-show="show"
+      @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
+      :page-sizes="[1,5,10,20,50]"
       :page-size="pageConfig.count"
+      layout="total, sizes, prev, pager, next, jumper"
       :total="pageConfig.totalPage"
-      layout="prev, pager, next"
     ></el-pagination>
   </div>
 </template>
 <script>
-import { getMovieList } from '@/api/movie'
+import { getMovieList } from "@/api/movie";
 
 export default {
-  name: 'douban',
+  name: "douban",
   data() {
     return {
       moviedata: null,
       pageConfig: {
         start: 0,
         count: 5,
-        totalPage: 250,
+        totalPage: 250
       },
       listLoading: false,
       show: false
-    }
+    };
   },
   methods: {
+    handleSizeChange(val) {
+      this.pageConfig.count = val;
+      this.getMoive(this.pageConfig.start, val);
+    },
+    getImages(_url) {
+      if (_url !== undefined) {
+        let _u = _url.substring(7);
+        return "https://images.weserv.nl/?url=" + _u;
+      }
+    },
     handleCurrentChange(val) {
       this.getMoive((val - 1) * this.pageConfig.count, this.pageConfig.count);
     },
-    getMoive: function (start, count) {
-      this.listLoading = true
-      getMovieList('/mapi/top250', { start: start, count: count }).then(res => {
-        this.moviedata = res.subjects
-        this.listLoading = false
-        this.show = true
-      })
+    getMoive: function(start, count) {
+      this.listLoading = true;
+      getMovieList("/mapi/top250", {
+        start: start,
+        count: count,
+        apikey: "0df993c66c0c636e29ecbb5344252a4a"
+      }).then(res => {
+        this.moviedata = res.subjects;
+        this.listLoading = false;
+        this.show = true;
+      });
     }
   },
 
-  created: function () {
-    this.getMoive(0, 5)
+  created: function() {
+    this.getMoive(0, 5);
   }
-}
+};
 </script>
 <style>
 #container {
@@ -114,6 +132,6 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
-  font-size: 12px
+  font-size: 12px;
 }
 </style>
